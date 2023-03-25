fe61e73ebbf0f5b2f09698bd907c6664585d374c
brightlightkim <k2289@byu.edu>
2023-03-24 20:50:45 -0600

    Updated the Extension

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,12 @@
+function notifyContentPage(e) {
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        if (tabs.length == 0) {
+            console.log("could not send message to the current tab");
+        } else {
+            const sendingToContent = browser.tabs.sendMessage(tabs[0].id, {message: "summarize"});
+            sendingToContent.then(handleResponse, handleError);
+        }
+    });
+}
+
 console.log("Hello World!", browser);
